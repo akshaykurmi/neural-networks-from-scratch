b65ffc5c8f86be9ec9e5f35eb7466a7b715be640
Akshay Kurmi <akskurmi@gmail.com>
2019-08-23 17:13:57 -0400

    Calculate fan_in and fan_out for > 2 dimensions

--- nnfs/initializers/glorot.py
+++ nnfs/initializers/glorot.py
@@ -4,7 +4,8 @@ import numpy as np
 class GlorotUniform:
     @staticmethod
     def initialize(shape):
-        fan_in, fan_out = shape[0], shape[1]
+        fan_in = shape[0] if len(shape) == 2 else shape[1] * np.prod(shape[2:])
+        fan_out = shape[1] if len(shape) == 2 else shape[0] * np.prod(shape[2:])
         scale = np.sqrt(6.0 / (fan_in + fan_out))
         return np.random.uniform(-scale, scale, shape)
 
@@ -12,6 +13,7 @@ class GlorotUniform:
 class GlorotNormal:
     @staticmethod
     def initialize(shape):
-        fan_in, fan_out = shape[0], shape[1]
+        fan_in = shape[0] if len(shape) == 2 else shape[1] * np.prod(shape[2:])
+        fan_out = shape[1] if len(shape) == 2 else shape[0] * np.prod(shape[2:])
         scale = np.sqrt(2.0 / (fan_in + fan_out))
-        return np.random.randn(shape) * scale
+        return np.random.randn(*shape) * scale
--- nnfs/initializers/he.py
+++ nnfs/initializers/he.py
@@ -4,7 +4,7 @@ import numpy as np
 class HeUniform:
     @staticmethod
     def initialize(shape):
-        fan_in = shape[0]
+        fan_in = shape[0] if len(shape) == 2 else shape[1] * np.prod(shape[2:])
         scale = np.sqrt(6.0 / fan_in)
         return np.random.uniform(-scale, scale, shape)
 
@@ -12,6 +12,6 @@ class HeUniform:
 class HeNormal:
     @staticmethod
     def initialize(shape):
-        fan_in = shape[0]
+        fan_in = shape[0] if len(shape) == 2 else shape[1] * np.prod(shape[2:])
         scale = np.sqrt(2.0 / fan_in)
-        return np.random.randn(shape) * scale
+        return np.random.randn(*shape) * scale
