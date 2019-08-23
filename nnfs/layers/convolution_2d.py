55441e023847fd974ec0c6fd43dbc3c86ed41515
Akshay Kurmi <akskurmi@gmail.com>
2019-08-23 10:58:42 -0400

    Implement convolution 2d layer

--- nnfs/layers/convolution_2d.py
+++ nnfs/layers/convolution_2d.py
@@ -3,12 +3,12 @@ from .base import Layer
 
 
 class Convolution2D(Layer):
-    def __init__(self, num_filters, kernel_size, strides, padding, activation, weights_initializer, bias_initializer,
+    def __init__(self, num_filters, filter_size, stride, padding, activation, weights_initializer, bias_initializer,
                  input_shape=None):
         super().__init__()
         self.num_filters = num_filters
-        self.kernel_size = kernel_size
-        self.strides = strides
+        self.filter_size = filter_size
+        self.stride = stride
         self.padding = padding
         self.input_shape = input_shape
         self.output_shape = None
@@ -16,14 +16,17 @@ class Convolution2D(Layer):
         self.bias_initializer = bias_initializer
         self.weights_initializer = weights_initializer
         self.W, self.dW, self.b, self.db = None, None, None, None
-        self.most_recent_propagated_input = None
+        self.cache = {}
 
     def _initialize_parameters(self):
-        batch_size, width, height, channels = self.input_shape
-        output_width = 1 + (width + 2 * self.padding[0] - self.kernel_size[0]) // self.strides[0]
-        output_height = 1 + (height + 2 * self.padding[1] - self.kernel_size[1]) // self.strides[1]
-        self.output_shape = (batch_size, self.num_filters, output_width, output_height)
-        self.W = self.weights_initializer.initialize((self.num_filters, *self.kernel_size, channels))
+        batch_size, channels, height, width = self.input_shape
+        filter_height, filter_width = self.filter_size
+        assert (height + 2 * self.padding - filter_height) % self.stride == 0
+        assert (width + 2 * self.padding - filter_width) % self.stride == 0
+        output_height = 1 + (height + 2 * self.padding - filter_height) // self.stride
+        output_width = 1 + (width + 2 * self.padding - filter_width) // self.stride
+        self.output_shape = (batch_size, self.num_filters, output_height, output_width)
+        self.W = self.weights_initializer.initialize((self.num_filters, channels, filter_height, filter_width))
         self.b = self.bias_initializer.initialize((self.num_filters,))
 
     def make_first_layer(self):
@@ -35,54 +38,57 @@ class Convolution2D(Layer):
         self.input_shape = prev_layer.output_shape
         self._initialize_parameters()
 
-    def forward(self, propagated_input, *args, **kwargs):
-        pad_widths = ((0, 0), 2 * (self.padding[0],), 2 * (self.padding[1],), (0, 0))
-        padded_input = np.pad(propagated_input, pad_widths, mode="constant")
-        self.most_recent_propagated_input = padded_input
-        weighted_output = []
-        batch_size = self.input_shape[0]
-        for b in range(batch_size):
-            frame = padded_input[b]
-            filters = []
-            for f in range(self.num_filters):
-                filter_ = self.W[f]
-                strides_view_shape = tuple(1 + np.subtract(frame.shape[:-1], filter_.shape[:-1])) + filter_.shape
-                a, b, c = frame.strides
-                conv_windows = np.lib.stride_tricks.as_strided(frame, strides_view_shape, (b, a, a, b, c))
-                filters.append(np.einsum("klm,ijklm->ij", filter_, conv_windows) + self.b[f])
-            weighted_output.append(filters)
-        activation_output = self.activation.compute(weighted_output)
-        return activation_output
+    def forward(self, X, *args, **kwargs):
+        _, _, output_height, output_width = self.output_shape
+        batch_size = X.shape[0]
+        X_cols = self._im2col(X)
+        output = self.W.reshape((self.num_filters, -1)).dot(X_cols) + self.b.reshape(-1, 1)
+        output = output.reshape(self.num_filters, output_height, output_width, batch_size)
+        output = output.transpose(3, 0, 1, 2)
+        self.cache = {"X_cols": X_cols}
+        return self.activation.compute(output)
 
-    def backward(self, back_propagated_gradients):
-        activation_gradients = back_propagated_gradients * self.activation.gradients()
-        result = []
-        for b in range(self.input_shape[0]):
-            xx = self.most_recent_propagated_input[b]
-            r = []
-            for f in range(self.num_filters):
-                ff = self.W[f]
-                gg = activation_gradients[b][f]
-                view_shape = ff.shape[:-1] + tuple(1 + np.subtract(xx.shape[:-1], ff.shape[:-1])) + (ff.shape[-1],)
-                a, b, c = xx.strides
-                conv_windows = np.lib.stride_tricks.as_strided(xx, view_shape, (b, a, a, b, c))
-                cc = np.einsum("lk,ijklm->jim", gg, conv_windows)
-                r.append(cc)
-            result.append(r)
-        self.dW = np.mean(result, axis=0)
-        self.db = np.mean(activation_gradients, axis=(0, 2, 3)).reshape(self.b.shape)
-        if not self.is_first_layer:
-            layer_grads = np.zeros(self.input_shape)
-            for b in range(self.input_shape[0]):
-                xx = layer_grads[b]
-                for f in range(self.num_filters):
-                    ff = self.W[f]
-                    gg = activation_gradients[b][f]
-                    view_shape = tuple(1 + np.subtract(xx.shape[:-1], ff.shape[:-1])) + ff.shape
-                    a, b, c = xx.strides
-                    conv_windows = np.lib.stride_tricks.as_strided(xx, view_shape, (b, a, a, b, c))
-                    conv_windows += np.tile(ff, (*gg.shape, 1, 1, 1)) * gg.reshape((*gg.shape, 1, 1, 1))
-            return layer_grads
+    def backward(self, gradients):
+        gradients = gradients * self.activation.gradients()
+        self.db = np.sum(gradients, axis=(0, 2, 3))
+        gradients = gradients.transpose(1, 2, 3, 0).reshape(self.num_filters, -1)
+        self.dW = gradients.dot(self.cache["X_cols"].T).reshape(self.W.shape)
+        dX_cols = self.W.reshape(self.num_filters, -1).T.dot(gradients)
+        return self._col2im(dX_cols)
+
+    def _im2col(self, im):
+        filter_height, filter_width = self.filter_size
+        channels = self.input_shape[1]
+        pad_width = ((0, 0), (0, 0), (self.padding, self.padding), (self.padding, self.padding))
+        padded = np.pad(im, pad_width, mode='constant')
+        i, j, k = self._im2col_indices()
+        cols = padded[:, k, i, j]
+        return cols.transpose(1, 2, 0).reshape(filter_height * filter_width * channels, -1)
+
+    def _col2im(self, cols):
+        filter_height, filter_width = self.filter_size
+        batch_size, channels, height, width = self.input_shape
+        padded_height, padded_width = height + 2 * self.padding, width + 2 * self.padding
+        i, j, k = self._im2col_indices()
+        cols = cols.reshape(channels * filter_height * filter_width, -1, batch_size).transpose(2, 0, 1)
+        im = np.zeros((batch_size, channels, padded_height, padded_width), dtype=cols.dtype)
+        np.add.at(im, (slice(None), k, i, j), cols)
+        if self.padding == 0:
+            return im
+        return im[:, :, self.padding:-self.padding, self.padding:-self.padding]
+
+    def _im2col_indices(self):
+        filter_height, filter_width = self.filter_size
+        output_height, output_width = self.output_shape[-2:]
+        channels = self.input_shape[1]
+        i0 = np.tile(np.repeat(np.arange(filter_height), filter_width), channels)
+        i1 = self.stride * np.repeat(np.arange(output_height), output_width)
+        j0 = np.tile(np.arange(filter_width), filter_height * channels)
+        j1 = self.stride * np.tile(np.arange(output_width), output_height)
+        i = i0.reshape(-1, 1) + i1.reshape(1, -1)
+        j = j0.reshape(-1, 1) + j1.reshape(1, -1)
+        k = np.repeat(np.arange(channels), filter_height * filter_width).reshape(-1, 1)
+        return i, j, k
 
     @property
     def parameters_and_gradients(self):
